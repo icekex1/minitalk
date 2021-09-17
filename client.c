5e00e6f8cfa4e17fe16c95ada4644b617f72e0a7
tzeck <tzeck@student.42heilbronn.de>
2021-09-17 12:16:43 +0200

    fixed communication

--- client.c
+++ client.c
@@ -6,33 +6,31 @@
 /*   By: tzeck <tzeck@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 10:27:01 by tzeck             #+#    #+#             */
-/*   Updated: 2021/09/16 11:18:38 by tzeck            ###   ########.fr       */
+/*   Updated: 2021/09/17 12:15:12 by tzeck            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 #include <stdio.h>
 #include <string.h>
+#include <signal.h>
 
 int	send_bit(int n, int pid)
 {
-	usleep(50);
+	usleep(10000);
 	if (n == 0)
 		kill(pid, SIGUSR1); // either call 1 or 2 to let server know if bit is 1 or 0 (what a dumb system to share data wtf)
 	else if (n == 1)
 		kill(pid, SIGUSR2);
-		return (1);
+	return (1);
 }
 
 int	base_two(char c, int pid) //put stuff liek string and pid in struct
 {
 	int n;
 	int i;
-	int j;
 
 	i = 0;
-	j = 0;
-	// s = calloc(9, 1);
 	//make stuff to binary
 	while (i < 8)
 	{
@@ -41,7 +39,6 @@ int	base_two(char c, int pid) //put stuff liek string and pid in struct
 			n = 1;
     	send_bit(n, pid);
 		i++;
-		j++;
 	}
 	// printf("%s", s);
 	return (1);
@@ -51,17 +48,10 @@ int	main(int argc, char **argv)
 {
 	int	pid;
 	int i;
-	char *s = "a";
+	char *s;
 
 	pid = atoi(argv[1]);
-	// s = ;//strdup(argv[2]);
-	// printf("pid from server: %s\n", argv[1]);
-	// printf("pid from client: %d\n", getpid());
-	// printf("message from client: %s\n", argv[2]);
-	sleep(1);
-	kill(pid, SIGUSR1);
-	// sleep(1);
-	// kill(pid, SIGUSR2);
+	s = strdup(argv[2]);
 	while (s[i] != '\0')
 	{
 		base_two(s[i], pid);
--- server.c
+++ server.c
@@ -6,60 +6,105 @@
 /*   By: tzeck <tzeck@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/15 10:26:57 by tzeck             #+#    #+#             */
-/*   Updated: 2021/09/16 11:17:27 by tzeck            ###   ########.fr       */
+/*   Updated: 2021/09/17 12:14:43 by tzeck            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include <stdio.h>
 #include <signal.h>
+#include <string.h>
+#include "minitalk.h"
 
-// void	handle_sigtstp(int sig)
-// {
-// 	printf("stop not allowed\n");
-// }
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	unsigned int	src_len;
+	unsigned int	dst_len;
+	unsigned int	i;
 
-// void	handle_sigcont(int sig)
-// {
-// 	fflush(stdout);
-// 	printf("server pid: 666\n");
-// }
+	src_len = strlen(src);
+	dst_len = 0;
+	if (dstsize == 0)
+		return (src_len);
+	while (dst_len < dstsize && dst[dst_len])
+		dst_len++;
+	if (dstsize <= dst_len)
+		return (dstsize + src_len);
+	i = 0;
+	while (dstsize && (--dstsize - dst_len) && src[i])
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	return (src_len + dst_len);
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	int	i;
+
+	if (s != NULL)
+	{
+		i = 0;
+		while (s[i])
+		{
+			write(fd, &s[i], sizeof(char));
+			i++;
+		}
+	}
+}
+
+char	*print_string(char *string)
+{
+	ft_putstr_fd(string, 1);
+	free(string);
+	return (NULL);
+}
 
 void	handle_sigusr(int sig)
 {
+	static char c;
+	static int bits;
+	static char *string;
+	
+	bits = 0;
+	c = 0xFF;
 	if (sig == SIGUSR1)
-		printf("0");
-	else
-		printf("1");
+		c ^= 0x80 >> bits;
+	else if (sig == SIGUSR2)
+		c |= 0x80 >> bits;
+	if (++bits == 8)
+	{
+		if (c)
+			ft_strlcat(string, &c, 1);
+		else
+		{
+			string = print_string(string);
+			bits = 0;
+			c = 0xFF;
+		}
+	}
+
+	// if (sig == SIGUSR1)
+	// 	write(1, "0", 1);
+	// else if (sig == SIGUSR2)
+	// 	write(1, "1", 1);
 }
 
 int	main(void)
 {
-#pragma region kill
-	// struct sigaction sa;
-	// sa.sa_handler = &handle_sigcont;
-	// sigaction(SIGCONT, &sa, NULL);
-
-	// signal(SIGTSTP, &handle_sigtstp);
-
-	// printf("server pid: %d\n", getpid()); // print out pid for client
-	// while (1)
-	// {
-	// 	printf("text from loop\n");
-	// 	usleep(1000000);
-	// }
-#pragma endregion
-	// struct sigaction sa;
-
-	// sa.__sigaction_u.__sa_handler = &handle_sigusr;
-	// sigaction(SIGUSR1, &sa, NULL);
-	// sigaction(SIGUSR2, &sa, NULL);
-	int pid = getpid();
-
+	struct sigaction sa;
+	pid_t pid;
+	static char *s;
+	
+	pid = getpid();
+	sa.__sigaction_u.__sa_handler = &handle_sigusr;
 	printf("server pid: %d\n", pid); // print out pid for client
-	signal(SIGUSR1, handle_sigusr);
-	signal(SIGUSR2, handle_sigusr);
-	while(1) // to get multible calls without stopping
+	while (1) // to get multible calls without stopping	
+	{
+		sigaction(SIGUSR2, &sa, NULL);
+		sigaction(SIGUSR1, &sa, NULL);
 		pause(); //waits until a signal arrives
+	}
 	return (1);
 }
